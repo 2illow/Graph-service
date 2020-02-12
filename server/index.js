955e9e5daea1848d171fc7df00b1e2e993f56921
Kevin Lonergan <kevinlonergan@Kevins-MacBook-Pro-3.local>
2020-02-12 12:00:15 -0800

    add and comment out console log of sample data

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+//const util = require('util');
 const app = express();
 const port = 3003;
 const db = require('../database/schema.js');
@@ -11,6 +11,7 @@ app.use(express.static('public'));
 app.get('/seed', (req, res) => {
   db.save(() => {
     db.retrieve((docs) => {
+      // console.log(util.inspect(docs, false, null, true /* enable colors */))
       res.json(docs);
     });
   });
